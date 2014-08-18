a91f999a54381d1aa7f154e512ddcb3fb7a4c80e
Felin <elisummer@live.com.ph>
2014-08-18 18:29:38 +0800

    third push err

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+from polls.models import Choice, Poll
+
+
+
+class HomeView(generic.TemplateView):
+    template_name = "polls/home.html"
+    
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_poll_list'
+
+    def get_queryset(self):
+        return Poll.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+        # return Poll.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Poll.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Poll
+    template_name = 'polls/results.html'
+
+# def index(request):
+#     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+#     context = {'latest_poll_list': latest_poll_list}
+#     return render(request, 'polls/index.html', context)
+
+# def detail(request, poll_id):
+#     try:
+#         poll = Poll.objects.get(pk=poll_id)
+#     except Poll.DoesNotExist:
+#         raise Http404
+#     return render(request, 'polls/detail.html', {'poll': poll})
+
+# def results(request, poll_id):
+#     poll = get_objects_or_404(Poll, pk=poll_id)
+#     return render (request, 'polls/results.html', {'poll': poll})
+
+def vote(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (keyError, Choice.DoesNotExist) :
+        return render (request, 'polls/detail.html', {
+            'poll': p,
+            'error_message': "You did'nt select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
